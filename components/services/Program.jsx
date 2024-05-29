d953d43f136e1a7f5ab4c5644158d6e8cc4f1f25
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2024-05-29 11:58:49 -0400

    join

--- components/services/Program.jsx
+++ components/services/Program.jsx
@@ -1,23 +1,20 @@
 import React from 'react';
-
 import styles from "./stylesheets/Program.module.css";
 
 const Program = () => {
   return (
-    
-      <div className={styles.container}>
-        <div className={styles.info}>
-          <h3 className={styles.title}>Réunion d'adoration</h3>
-          <h4 className={styles.time}>DIMANCHE - 11:00</h4>
-          <p className={styles.description}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nam incidunt magni distinctio error beatae non, repellat cumque explicabo earum et aperiam atque esse. Quos doloribus eligendi repellendus necessitatibus nihil tempore!</p>
-        </div>
-        <div className={styles.info}>
-          <h3 className={styles.title}>Réunion de prière</h3>
-          <h4 className={styles.time}>MERCREDI - 17:00</h4>
-          <p className={styles.description}>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ad temporibus molestias et nam, magnam amet accusamus aspernatur! Minus quisquam a tempora saepe explicabo? Saepe, ratione consectetur quibusdam asperiores eligendi inventore!</p>
-        </div>
+    <div className={styles.container}>
+      <div className={styles.info}>
+        <h3 className={styles.title}>Culte du dimanche</h3>
+        <h4 className={styles.time}>DIMANCHE - 11:00</h4>
+        <p className={styles.description}>Joignez-vous à nous pour un moment de louange et d'adoration le dimanche à 11h.</p>
       </div>
-   
+      <div className={styles.info}>
+        <h3 className={styles.title}>Réunion de prière</h3>
+        <h4 className={styles.time}>MERCREDI - 17:00</h4>
+        <p className={styles.description}>Rejoignez-nous pour un temps de prière et de partage le mercredi à 17h.</p>
+      </div>
+    </div>
   );
 };
 
