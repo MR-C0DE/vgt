a981dc3778627c3fd3d5eaecd7bbf5b445b46f62
MR-C0DE <80490263+MR-C0DE@users.noreply.github.com>
2024-09-01 07:34:18 -0400

    data

--- components/history/Story.jsx
+++ components/history/Story.jsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useLanguage } from '../contexts/LanguageContext';
+import { dataStory } from './data/dataStory';
 import styles from './stylesheets/Story.module.css';
 
+
 const Story = () => {
+  const { language } = useLanguage();
+
   return (
     <div className={styles.historyContainer}>
      
@@ -12,7 +17,7 @@ const Story = () => {
       
       <div className={styles.historyText}>
         <p>
-          Voice of God Tabernacle a été fondée par le Pasteur Jean-Claude Dupuy en 1995, après avoir déménagé dans la capitale, Ottawa, la même année. N'ayant trouvé aucune congrégation chrétienne croyant au Message de l'heure, il se sentit obligé de commencer de modestes réunions avec sa famille et d'autres croyants dans le sous-sol de sa maison. Il croyait fermement que c'était un privilège de ramener les enfants de Dieu et sa famille, qui adoraient avec lui, à la source (la Voix, Malachie 4 versets 5 & 6) que le Seigneur Jésus-Christ avait prévue avant la fondation du monde.
+        {dataStory[language][0]}
         </p>
        </div>
     </div>
---
+++ components/history/data/dataStory.js
@@ -0,0 +1,8 @@
+export const dataStory = {
+  en: [
+    "Voice of God Tabernacle was founded by Pastor Jean-Claude Dupuy in 1995, after he moved to the capital, Ottawa, the same year. Finding no Christian congregation that believed in the Message of the Hour, he felt compelled to start modest meetings with his family and other believers in the basement of his home. He firmly believed that it was a privilege to bring God’s children and his family, who worshipped with him, back to the source (the Voice, Malachi 4:5-6) that the Lord Jesus Christ had ordained before the foundation of the world.",
+  ],
+  fr: [
+    "Voice of God Tabernacle a été fondée par le Pasteur Jean-Claude Dupuy en 1995, après avoir déménagé dans la capitale, Ottawa, la même année. N'ayant trouvé aucune congrégation chrétienne croyant au Message de l'heure, il se sentit obligé de commencer de modestes réunions avec sa famille et d'autres croyants dans le sous-sol de sa maison. Il croyait fermement que c'était un privilège de ramener les enfants de Dieu et sa famille, qui adoraient avec lui, à la source (la Voix, Malachie 4 versets 5 & 6) que le Seigneur Jésus-Christ avait prévue avant la fondation du monde.",
+  ],
+};
